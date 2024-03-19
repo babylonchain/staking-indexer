95ce99e3e1a57b27fe4b1f26f3cca075cda8427d
Fangyu Gai <greferry@gmail.com>
2024-03-19 22:06:37 +0800

    simplify btc scanner

--- btcscanner/btc_scanner_test.go
+++ btcscanner/btc_scanner_test.go
@@ -2,6 +2,7 @@ package btcscanner_test
 
 import (
 	"math/rand"
+	"sync"
 	"testing"
 
 	"github.com/babylonchain/babylon/testutil/datagen"
@@ -22,17 +23,17 @@ func FuzzBootStrap(f *testing.F) {
 
 	f.Fuzz(func(t *testing.T, seed int64) {
 		r := rand.New(rand.NewSource(seed))
-		k := uint64(6)
+		cfg := config.DefaultBTCScannerConfig()
+		k := cfg.ConfirmationDepth
 		// Generate a random number of blocks
 		numBlocks := datagen.RandomIntOtherThan(r, 0, 50) + k // make sure we have at least k+1 entry
 		chainIndexedBlocks := vdatagen.GetRandomIndexedBlocks(r, numBlocks)
-		baseHeight := chainIndexedBlocks[0].Height
+		lastConfirmedHeight := chainIndexedBlocks[0].Height - 1
 		bestHeight := chainIndexedBlocks[len(chainIndexedBlocks)-1].Height
 
 		ctl := gomock.NewController(t)
 		mockBtcClient := mocks.NewMockBTCClient(ctl)
 		confirmedBlocks := chainIndexedBlocks[:numBlocks-k]
-		mockBtcClient.EXPECT().MustSubscribeBlocks().Return().AnyTimes()
 		mockBtcClient.EXPECT().GetBestBlock().Return(nil, uint64(bestHeight), nil)
 		for i := 0; i < int(numBlocks); i++ {
 			mockBtcClient.EXPECT().GetBlockByHeight(gomock.Eq(uint64(chainIndexedBlocks[i].Height))).
@@ -45,16 +46,22 @@ func FuzzBootStrap(f *testing.F) {
 			ConfChan:  make(chan *chainntnfs.TxConfirmation),
 		}
 
-		cfg := config.DefaultBTCScannerConfig()
-		btcScanner, err := btcscanner.NewBTCScanner(cfg, zap.NewNop(), mockBtcClient, mockBtcNotifier, uint64(baseHeight))
+		btcScanner, err := btcscanner.NewBTCScanner(cfg, zap.NewNop(), mockBtcClient, mockBtcNotifier, uint64(lastConfirmedHeight))
 		require.NoError(t, err)
 
+		var wg sync.WaitGroup
+		wg.Add(1)
 		go func() {
+			defer wg.Done()
 			for i := 0; i < len(confirmedBlocks); i++ {
 				b := <-btcScanner.ConfirmedBlocksChan()
 				require.Equal(t, confirmedBlocks[i].BlockHash(), b.BlockHash())
 			}
 		}()
-		btcScanner.Bootstrap()
+		err = btcScanner.Start()
+		require.NoError(t, err)
+
+		wg.Wait()
+		require.Equal(t, uint64(confirmedBlocks[len(confirmedBlocks)-1].Height), btcScanner.LastConfirmedHeight())
 	})
 }
--- config/btcscanner.go
+++ config/btcscanner.go
@@ -2,24 +2,20 @@ package config
 
 import (
 	"fmt"
+	"time"
 )
 
 const (
-	defaultBlockBufferSize   = 100
-	defaultCacheSize         = 100
-	defaultConfirmationDepth = 6
+	defaultPollingInterval   = 1 * time.Second
+	defaultConfirmationDepth = 10
 )
 
 type BTCScannerConfig struct {
-	BlockBufferSize   uint64 `long:"blockbuffersize" description:"the max number of BTC blocks in the buffer"`
-	CacheSize         uint64 `long:"cachesize" description:"max number of BTC blocks in the cache"`
-	ConfirmationDepth uint64 `long:"confirmationdepth" description:"the confirmation depth to consider a BTC block as confirmed"`
+	PollingInterval   time.Duration `long:"pollinginterval" description:"the time interval between each polling of new confirmed blocks"`
+	ConfirmationDepth uint64        `long:"confirmationdepth" description:"the confirmation depth to consider a BTC block as confirmed"`
 }
 
 func (cfg *BTCScannerConfig) Validate() error {
-	if cfg.CacheSize < defaultCacheSize {
-		return fmt.Errorf("btc-cache-size should not be less than %v", defaultCacheSize)
-	}
 	if cfg.ConfirmationDepth < defaultConfirmationDepth {
 		return fmt.Errorf("btc-confirmation-depth should not be less than %d", defaultConfirmationDepth)
 	}
@@ -28,8 +24,7 @@ func (cfg *BTCScannerConfig) Validate() error {
 
 func DefaultBTCScannerConfig() *BTCScannerConfig {
 	return &BTCScannerConfig{
-		BlockBufferSize:   defaultBlockBufferSize,
-		CacheSize:         defaultCacheSize,
+		PollingInterval:   defaultPollingInterval,
 		ConfirmationDepth: defaultConfirmationDepth,
 	}
 }
