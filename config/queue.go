414c3fc0a479483f281f4ea78e5308863a170c19
Fangyu Gai <greferry@gmail.com>
2024-04-24 22:09:01 +0800

    add new queue client config

--- config/queue.go
+++ config/queue.go
@@ -8,17 +8,21 @@ import (
 )
 
 const (
-	defaultQueueUser              = "user"
-	defaultQueuePassword          = "password"
-	defaultQueueUrl               = "localhost:5672"
-	defaultQueueProcessingTimeout = 5 * time.Second
+	defaultQueueUser                = "user"
+	defaultQueuePassword            = "password"
+	defaultQueueUrl                 = "localhost:5672"
+	defaultQueueProcessingTimeout   = 5 * time.Second
+	defaultQueueMsgMaxRetryAttempts = 10
+	defaultReQueueDelayTime         = 5 * time.Second
 )
 
 type QueueConfig struct {
-	User              string        `long:"user" description:"the user name of the queue"`
-	Password          string        `long:"password" description:"the password of the queue"`
-	Url               string        `long:"url" description:"the url of the queue"`
-	ProcessingTimeout time.Duration `long:"processingtimeout" description:"the process timeout of the queue"`
+	User                string        `long:"user" description:"the user name of the queue"`
+	Password            string        `long:"password" description:"the password of the queue"`
+	Url                 string        `long:"url" description:"the url of the queue"`
+	ProcessingTimeout   time.Duration `long:"processingtimeout" description:"the process timeout of the queue"`
+	MsgMaxRetryAttempts int32         `long:"msgmaxretryattempts" description:"the maximum number of times a message will be retried"`
+	ReQueueDelayTime    time.Duration `long:"requeuedelaytime" description:"the time a message will be hold in delay queue before sent to main queue again"`
 }
 
 func (cfg *QueueConfig) Validate() error {
@@ -38,6 +42,15 @@ func (cfg *QueueConfig) Validate() error {
 		return fmt.Errorf("invalid queue processing timeout")
 	}
 
+	if cfg.MsgMaxRetryAttempts <= 0 {
+		return fmt.Errorf("invalid queue message max retry attempts")
+	}
+
+	if cfg.ReQueueDelayTime <= 0 {
+		return fmt.Errorf(`invalid requeue delay time. 
+		It should be greater than 0, the unit is seconds`)
+	}
+
 	return nil
 }
 
@@ -47,14 +60,18 @@ func (cfg *QueueConfig) ToQueueClientConfig() *clicfg.QueueConfig {
 		QueuePassword:          cfg.Password,
 		Url:                    cfg.Url,
 		QueueProcessingTimeout: cfg.ProcessingTimeout,
+		MsgMaxRetryAttempts:    cfg.MsgMaxRetryAttempts,
+		ReQueueDelayTime:       cfg.ReQueueDelayTime,
 	}
 }
 
 func DefaultQueueConfig() *QueueConfig {
 	return &QueueConfig{
-		User:              defaultQueueUser,
-		Password:          defaultQueuePassword,
-		Url:               defaultQueueUrl,
-		ProcessingTimeout: defaultQueueProcessingTimeout,
+		User:                defaultQueueUser,
+		Password:            defaultQueuePassword,
+		Url:                 defaultQueueUrl,
+		ProcessingTimeout:   defaultQueueProcessingTimeout,
+		MsgMaxRetryAttempts: defaultQueueMsgMaxRetryAttempts,
+		ReQueueDelayTime:    defaultReQueueDelayTime,
 	}
 }
