57819f78cb14b6ef417f2c4bfc6a8058a118acda
Cirrus Gai <greferry@gmail.com>
2024-05-14 17:36:37 +0800

    chore: Add BTC unconfirmed block cache (#72)

---
+++ btcscanner/btc_cache.go
@@ -0,0 +1,173 @@
+package btcscanner
+
+import (
+	"fmt"
+	"sort"
+	"sync"
+
+	"github.com/babylonchain/staking-indexer/types"
+)
+
+const defaultMaxEntries = 100
+
+type BTCCache struct {
+	blocks     []*types.IndexedBlock
+	maxEntries uint64
+
+	sync.RWMutex
+}
+
+func NewBTCCache(maxEntries uint64) (*BTCCache, error) {
+	// if maxEntries is 0, it means that the cache is disabled
+	if maxEntries == 0 {
+		return nil, ErrInvalidMaxEntries
+	}
+
+	return &BTCCache{
+		blocks:     make([]*types.IndexedBlock, 0, maxEntries),
+		maxEntries: maxEntries,
+	}, nil
+}
+
+// Init initializes the cache with the given blocks. Input blocks should be sorted by height. Thread-safe.
+func (b *BTCCache) Init(ibs []*types.IndexedBlock) error {
+	b.Lock()
+	defer b.Unlock()
+
+	if len(ibs) > int(b.maxEntries) {
+		return ErrTooManyEntries
+	}
+
+	// check if the blocks are sorted by height
+	if sortedByHeight := sort.SliceIsSorted(ibs, func(i, j int) bool {
+		return ibs[i].Height < ibs[j].Height
+	}); !sortedByHeight {
+		return ErrUnsortedBlocks
+	}
+
+	for _, ib := range ibs {
+		if err := b.add(ib); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+// Add adds a new block to the cache. Thread-safe.
+func (b *BTCCache) Add(ib *types.IndexedBlock) error {
+	b.Lock()
+	defer b.Unlock()
+
+	return b.add(ib)
+}
+
+// Thread-unsafe version of Add
+func (b *BTCCache) add(ib *types.IndexedBlock) error {
+	l := b.size()
+	if l > b.maxEntries {
+		return fmt.Errorf("%w: current size is %d, max entries is %d", ErrTooManyEntries, l, b.maxEntries)
+	}
+	if l == b.maxEntries {
+		// dereference the 0-th block to ensure it will be garbage-collected
+		// see https://stackoverflow.com/questions/55045402/memory-leak-in-golang-slice
+		b.blocks[0] = nil
+		b.blocks = b.blocks[1:]
+	}
+
+	b.blocks = append(b.blocks, ib)
+
+	return nil
+}
+
+func (b *BTCCache) First() *types.IndexedBlock {
+	b.RLock()
+	defer b.RUnlock()
+
+	if b.size() == 0 {
+		return nil
+	}
+
+	return b.blocks[0]
+}
+
+func (b *BTCCache) Tip() *types.IndexedBlock {
+	b.RLock()
+	defer b.RUnlock()
+
+	if b.size() == 0 {
+		return nil
+	}
+
+	return b.blocks[len(b.blocks)-1]
+}
+
+// RemoveAll deletes all the blocks in cache
+func (b *BTCCache) RemoveAll() {
+	b.Lock()
+	defer b.Unlock()
+
+	b.blocks = []*types.IndexedBlock{}
+}
+
+// Size returns the size of the cache. Thread-safe.
+func (b *BTCCache) Size() uint64 {
+	b.RLock()
+	defer b.RUnlock()
+
+	return b.size()
+}
+
+// thread-unsafe version of Size
+func (b *BTCCache) size() uint64 {
+	return uint64(len(b.blocks))
+}
+
+func (b *BTCCache) GetAllBlocks() []*types.IndexedBlock {
+	b.RLock()
+	defer b.RUnlock()
+
+	l := len(b.blocks)
+	res := make([]*types.IndexedBlock, l)
+	copy(res, b.blocks)
+
+	return res
+}
+
+// GetLastBlocks returns the last k blocks
+func (b *BTCCache) GetLastBlocks(k int) []*types.IndexedBlock {
+	b.RLock()
+	defer b.RUnlock()
+
+	var res []*types.IndexedBlock
+	l := len(b.blocks)
+	if l <= k {
+		blocksCopy := make([]*types.IndexedBlock, l)
+		copy(blocksCopy, b.blocks)
+		res = blocksCopy
+	} else {
+		blocksCopy := make([]*types.IndexedBlock, k)
+		copy(blocksCopy, b.blocks[l-k:])
+		res = blocksCopy
+	}
+
+	return res
+}
+
+// TrimConfirmedBlocks keeps the last <=k blocks in the cache and returns the rest in the same order
+// the returned blocks are considered confirmed
+func (b *BTCCache) TrimConfirmedBlocks(k int) []*types.IndexedBlock {
+	b.Lock()
+	defer b.Unlock()
+
+	l := len(b.blocks)
+	if l <= k {
+		return nil
+	}
+
+	res := make([]*types.IndexedBlock, l-k)
+	copy(res, b.blocks)
+	b.blocks = b.blocks[l-k:]
+
+	return res
+}
---
+++ btcscanner/btc_cache_test.go
@@ -0,0 +1,124 @@
+package btcscanner_test
+
+import (
+	"math/rand"
+	"testing"
+
+	bbndatagen "github.com/babylonchain/babylon/testutil/datagen"
+	"github.com/stretchr/testify/require"
+
+	"github.com/babylonchain/staking-indexer/btcscanner"
+	"github.com/babylonchain/staking-indexer/testutils/datagen"
+)
+
+// FuzzBtcCache fuzzes the BtcCache type
+func FuzzBtcCache(f *testing.F) {
+	bbndatagen.AddRandomSeedsToFuzzer(f, 100)
+
+	f.Fuzz(func(t *testing.T, seed int64) {
+		r := rand.New(rand.NewSource(seed))
+
+		// Create a new cache
+		maxEntries := bbndatagen.RandomInt(r, 1000) + 2 // make sure we have at least 2 entries
+
+		// 1/10 times generate invalid maxEntries
+		invalidMaxEntries := false
+		if bbndatagen.OneInN(r, 10) {
+			maxEntries = 0
+			invalidMaxEntries = true
+		}
+
+		cache, err := btcscanner.NewBTCCache(maxEntries)
+		if err != nil {
+			if !invalidMaxEntries {
+				t.Fatalf("NewBTCCache returned error %s", err)
+			}
+			require.ErrorIs(t, err, btcscanner.ErrInvalidMaxEntries)
+			t.Skip("Skipping test with invalid maxEntries")
+		}
+
+		// Generate a random number of blocks
+		numBlocks := bbndatagen.RandomIntOtherThan(r, 0, int(maxEntries)) // make sure we have at least 1 entry
+
+		// 1/10 times generate invalid number of blocks
+		invalidNumBlocks := false
+		if bbndatagen.OneInN(r, 10) {
+			numBlocks = maxEntries + 1
+			invalidNumBlocks = true
+		}
+
+		ibs := datagen.GetRandomIndexedBlocks(r, numBlocks)
+
+		// Add all indexed blocks to the cache
+		err = cache.Init(ibs)
+		if err != nil {
+			if !invalidNumBlocks {
+				t.Fatalf("Cache init returned error %v", err)
+			}
+			require.ErrorIs(t, err, btcscanner.ErrTooManyEntries)
+			t.Skip("Skipping test with invalid numBlocks")
+		}
+
+		require.Equal(t, numBlocks, cache.Size())
+
+		k := r.Intn(int(maxEntries)) + 1
+		lastBlocks := cache.GetLastBlocks(k)
+		l := cache.Size()
+		cacheAllBlocks := cache.GetAllBlocks()
+		if uint64(k) <= l {
+			require.Equal(t, k, len(lastBlocks))
+			for i, b := range cacheAllBlocks[int(l)-k:] {
+				require.Equal(t, b, lastBlocks[i])
+			}
+		} else {
+			require.Equal(t, int(l), len(lastBlocks))
+			for i, b := range cacheAllBlocks {
+				require.Equal(t, b, lastBlocks[i])
+			}
+		}
+
+		// Add random blocks to the cache
+		addCount := bbndatagen.RandomIntOtherThan(r, 0, 1000)
+		prevCacheHeight := cache.Tip().Height
+		cacheBlocksBeforeAddition := cache.GetAllBlocks()
+		blocksToAdd := datagen.GetRandomIndexedBlocksFromHeight(r, addCount, cache.Tip().Height, cache.Tip().BlockHash())
+		for _, ib := range blocksToAdd {
+			err := cache.Add(ib)
+			require.NoError(t, err)
+		}
+		require.Equal(t, prevCacheHeight+int32(addCount), cache.Tip().Height)
+		require.Equal(t, blocksToAdd[addCount-1], cache.Tip())
+
+		// ensure block heights in cache are in increasing order
+		var heights []int32
+		for _, ib := range cache.GetAllBlocks() {
+			heights = append(heights, ib.Height)
+		}
+		require.IsIncreasing(t, heights)
+
+		// we need to compare block slices before and after addition, there are 3 cases to consider:
+		// if addCount+numBlocks>=maxEntries then
+		// 1. addCount >= maxEntries
+		// 2. addCount < maxEntries
+		// else
+		// 3. addCount+numBlocks < maxEntries
+		// case 2 and 3 are the same, so below is simplified version
+		cacheBlocksAfterAddition := cache.GetAllBlocks()
+		if addCount >= maxEntries {
+			// cache contains only new blocks, so compare all cache blocks with slice of blocksToAdd
+			require.Equal(t, blocksToAdd[addCount-maxEntries:], cacheBlocksAfterAddition)
+		} else {
+			// cache contains both old and all the new blocks, so we need to compare
+			// blocksToAdd with slice of cacheBlocksAfterAddition and also compare
+			// slice of cacheBlocksBeforeAddition with slice of cacheBlocksAfterAddition
+
+			// compare new blocks
+			newBlocksInCache := cacheBlocksAfterAddition[len(cacheBlocksAfterAddition)-int(addCount):]
+			require.Equal(t, blocksToAdd, newBlocksInCache)
+
+			// comparing old blocks
+			oldBlocksInCache := cacheBlocksAfterAddition[:len(cacheBlocksAfterAddition)-int(addCount)]
+			require.Equal(t, cacheBlocksBeforeAddition[len(cacheBlocksBeforeAddition)-(len(cacheBlocksAfterAddition)-int(addCount)):], oldBlocksInCache)
+		}
+	})
+}
