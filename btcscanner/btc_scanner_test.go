508d518a8dafdc949706006cece880911178234d
Fangyu Gai <greferry@gmail.com>
2024-05-15 10:48:28 +0800

    stash

--- btcscanner/btc_scanner_test.go
+++ btcscanner/btc_scanner_test.go
@@ -15,6 +15,7 @@ import (
 	"github.com/babylonchain/staking-indexer/btcscanner"
 	"github.com/babylonchain/staking-indexer/testutils/datagen"
 	"github.com/babylonchain/staking-indexer/testutils/mocks"
+	"github.com/babylonchain/staking-indexer/types"
 )
 
 func FuzzPollConfirmedBlocks(f *testing.F) {
@@ -62,6 +63,87 @@ func FuzzPollConfirmedBlocks(f *testing.F) {
 			}
 		}()
 		err = btcScanner.Start(uint64(startHeight))
+		require.NoError(t, err)
+		defer func() {
+			err := btcScanner.Stop()
+			require.NoError(t, err)
+		}()
+
+		wg.Wait()
+		require.Equal(t, uint64(confirmedBlocks[len(confirmedBlocks)-1].Height), btcScanner.LastConfirmedHeight())
+	})
+}
+
+func FuzzReorgBlocks(f *testing.F) {
+	bbndatagen.AddRandomSeedsToFuzzer(f, 10)
+
+	f.Fuzz(func(t *testing.T, seed int64) {
+		r := rand.New(rand.NewSource(seed))
+		versionedParams := datagen.GenerateGlobalParamsVersions(r, t)
+		k := uint64(versionedParams.ParamsVersions[0].ConfirmationDepth)
+
+		// Generate a common prefix with random number of blocks
+		commonPrefixNum := bbndatagen.RandomIntOtherThan(r, 0, 10) + k
+		commonPrefixChain := datagen.GetRandomIndexedBlocks(r, commonPrefixNum)
+		commonParent := commonPrefixChain[len(commonPrefixChain)-1]
+		initialChainNum := bbndatagen.RandomIntOtherThan(r, 0, int(k)) // length shorter than k
+		initialChain := datagen.GetRandomIndexedBlocksFromHeight(r, initialChainNum, commonParent.Height, commonParent.BlockHash())
+		reorgChainNum := bbndatagen.RandomIntOtherThan(r, 0, 10) + k
+		reorgChain := datagen.GetRandomIndexedBlocksFromHeight(r, reorgChainNum, commonParent.Height, commonParent.BlockHash())
+
+		canonicalChain := make([]*types.IndexedBlock, 0)
+		canonicalChain = append(canonicalChain, commonPrefixChain...)
+		canonicalChain = append(canonicalChain, reorgChain...)
+		confirmedBlocks := canonicalChain[:len(canonicalChain)-int(k)+1]
+		epochChan := make(chan *chainntnfs.BlockEpoch, len(reorgChain))
+		for _, b := range reorgChain {
+			blockHash := b.BlockHash()
+			blockEpoch := &chainntnfs.BlockEpoch{
+				Hash:        &blockHash,
+				Height:      b.Height,
+				BlockHeader: b.Header,
+			}
+			epochChan <- blockEpoch
+		}
+
+		ctl := gomock.NewController(t)
+		mockBtcClient := mocks.NewMockClient(ctl)
+		for _, b := range commonPrefixChain {
+			mockBtcClient.EXPECT().GetBlockByHeight(gomock.Eq(uint64(b.Height))).
+				Return(b, nil).AnyTimes()
+		}
+		for _, b := range initialChain {
+			mockBtcClient.EXPECT().GetBlockByHeight(gomock.Eq(uint64(b.Height))).
+				Return(b, nil)
+		}
+		for _, b := range reorgChain {
+			mockBtcClient.EXPECT().GetBlockByHeight(gomock.Eq(uint64(b.Height))).
+				Return(b, nil)
+		}
+		gomock.InOrder(
+			mockBtcClient.EXPECT().GetTipHeight().Times(1).Return(uint64(initialChain[len(initialChain)-1].Height), nil),
+			mockBtcClient.EXPECT().GetTipHeight().Times(1).Return(uint64(reorgChain[len(initialChain)-1].Height), nil),
+		)
+
+		mockBtcNotifier := &mock.ChainNotifier{
+			EpochChan: epochChan,
+			SpendChan: make(chan *chainntnfs.SpendDetail),
+			ConfChan:  make(chan *chainntnfs.TxConfirmation),
+		}
+
+		btcScanner, err := btcscanner.NewBTCScanner(versionedParams, zap.NewNop(), mockBtcClient, mockBtcNotifier)
+		require.NoError(t, err)
+
+		var wg sync.WaitGroup
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for i := 0; i < len(confirmedBlocks); i++ {
+				b := <-btcScanner.ConfirmedBlocksChan()
+				require.Equal(t, confirmedBlocks[i].BlockHash(), b.BlockHash())
+			}
+		}()
+		err = btcScanner.Start(uint64(commonPrefixChain[0].Height))
 		require.NoError(t, err)
 		defer func() {
 			err := btcScanner.Stop()
